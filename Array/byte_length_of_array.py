65b1763450c6fcf51b66df6794b3119dbc5bd90f
DEVHrishi <royhrishikesh408@gmail.com>
2022-05-15 21:39:07 +0530

    commit

--- Array/byte_length_of_array.py
+++ Array/byte_length_of_array.py
@@ -1,4 +1,4 @@
 from array import *
-arr = array('i', [1, 3, 5, 7, 9])
-print('Original array: '+str(arr))
-print("Length in bytes of one array item: "+str(arr.itemsize))
+arr_input = array('i', [1, 3, 5, 7, 9])
+print('Original array: '+str(arr_input))
+print("Length in bytes of one array item: "+str(arr_input.itemsize))
