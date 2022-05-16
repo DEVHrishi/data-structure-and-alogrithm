15178bc5dda8da3750e7f53ef9017c9e397267a5
DEVHrishi <royhrishikesh408@gmail.com>
2022-05-16 20:11:52 +0530

    commit

---
+++ Array/insert_item_in_position.py
@@ -0,0 +1,6 @@
+from array import *
+arr_input = array('i', [1,3,5,7,9])
+print('Original array: '+str(arr_input))
+print('Insert new value 4 before 3:')
+arr_input.insert(1, 4)
+print('New array:', str(arr_input))
---
+++ Array/number_of_occurrences.py
@@ -0,0 +1,4 @@
+from array import *
+arr_input = array('i', [1, 3, 5, 3, 7, 9, 3])
+print("Original array: " + str(arr_input))
+print("Number of occurrences of the number 3 in the said array: "+str(arr_input.count(3)))
