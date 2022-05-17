d9b1c495b0b1aa4ee904007748c9bbb009d0604c
DEVHrishi <royhrishikesh408@gmail.com>
2022-05-17 23:31:17 +0530

    commit

---
+++ Array/remove_specified_element.py
@@ -0,0 +1,6 @@
+from array import *
+arr_input = array('i',[1, 3, 5, 3, 7, 1, 9, 3])
+print("Original array:", str(arr_input))
+print('Remove the first occurrence of 3 from the said array:')
+arr_input.remove(3)
+print("New array:", str(arr_input))
