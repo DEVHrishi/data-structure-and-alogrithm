254e939037b47c70bbe8cc94fb5ffbeaf92d4be7
DEVHrishi <royhrishikesh408@gmail.com>
2022-05-30 22:15:53 +0530

    cmt

--- Array/Easy/Two-sum.py
+++ Array/Easy/Two-sum.py
@@ -8,17 +8,18 @@ def twoSum(num, target):
 
 print('output:', twoSum([2, 7, 11, 15], 9))
 
-#efficient approach tc=O(n) sc=O(1)
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap:
-                return [i, hashmap[complement]]
-            hashmap[nums[i]] = i
+# efficient approach tc=O(n) sc=O(1)
+
+
+def twoSum(nums, target):
+    hashmap = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in hashmap:
+            return [i, hashmap[complement]]
+        hashmap[nums[i]] = i
 
 
 input_list = [2, 8, 12, 15]
-ob1 = Solution()
-print(ob1.twoSum(input_list, 20))
+
+print(twoSum(input_list, 20))
