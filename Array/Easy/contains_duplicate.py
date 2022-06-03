beef5f60f06f44e0567ec787b4f6deb590ba6d5b
DEVHrishi <royhrishikesh408@gmail.com>
2022-06-03 23:57:34 +0530

    cmt

--- Array/Easy/contains_duplicate.py
+++ Array/Easy/contains_duplicate.py
@@ -1,10 +1,29 @@
-def duplicate(arr_input):
-    arr_input.sort()
-    for i in range(len(arr_input)):
-        if arr_input[i] == arr_input[i+1]:
+# brute force tc = O(n^2) sc = O(1)
+def duplicate(nums):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] == nums[j]:
+                return True
+    return False
+
+# use sort method tc = O(nlogn) sc = O(1)
+def duplicate(nums):
+    nums.sort()
+    for i in range(len(nums)-1):
+        if nums[i] == nums[i+1]:
+            return True
+    return False
+
+# use set() method tc = O(n) sc = O(n)
+def duplicate(nums):
+    return len(set(nums)) != len(nums)
+
+# use hash table method tc = O(n) sc = O(n)
+def duplicate(nums):
+    d = {}
+    for i in range(len(nums)):
+        if nums[i] in d:
             return True
         else:
-            return False
-    
-
-print('duplicate contains:',duplicate([1,2,3,5,7,9,10]))
+            d[nums[i]] = 1
+    return False
