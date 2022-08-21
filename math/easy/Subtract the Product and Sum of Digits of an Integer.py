488ae918075ecef580d3d5b8bdd3bdbd65111c6f
Hrishikesh roy <royhrishikesh408@gmail.com>
2022-08-21 18:20:27 +0530

    commit

---
+++ math/easy/Subtract the Product and Sum of Digits of an Integer.py
@@ -0,0 +1,17 @@
+'''Given an integer number n, return the difference between the product of its digits and the sum of its digits.'''
+
+#TC = O(n) SC = O(n)
+def subtractProductAndSum(self, n: int) -> int:
+        arr = [int(a) for a in str(n)]
+        x = 1
+        y = 0
+        for i in arr:
+            x = x * i
+            y = y + i
+        return x-y
+
+import numpy
+class Solution(object):
+    def subtractProductAndSum(self, n):
+        new = map(int, str(n))
+        return  numpy.prod(new) - sum(new)
