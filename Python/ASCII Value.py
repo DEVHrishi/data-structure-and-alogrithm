a5be1928d87d21812b81c35aa4f66ebd1670fe74
Hrishikesh roy <royhrishikesh408@gmail.com>
2023-01-14 11:07:48 +0530

    commit

---
+++ Python/ASCII Value.py
@@ -0,0 +1,4 @@
+n = input('Enter a character: ')
+ASCII_value = ord(n)
+
+print('ASCII value of n: ', ASCII_value)
---
+++ Python/HCF or GCD.py
@@ -0,0 +1,26 @@
+def compute_GCD(x, y):
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+
+    for i in range(1, smaller + 1):
+        if ((x%i == 0) and (y%i == 0)):
+            hcf = i
+    return hcf
+
+print(compute_GCD(54, 24))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
