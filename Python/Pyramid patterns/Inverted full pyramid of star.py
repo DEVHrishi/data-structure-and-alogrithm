e8a9aecb438f0495ae91107b8730042e8c9e8090
Hrishikesh roy <royhrishikesh408@gmail.com>
2023-02-25 00:27:47 +0530

    commit

---
+++ Python/Pyramid patterns/Inverted full pyramid of star.py
@@ -0,0 +1,16 @@
+'''
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+'''
+
+n = int(input("Enter the number of rows: "))
+
+for i in range(n, 0, -1):
+    for j in range(n-i):
+        print(" ", end=' ')
+    for j in range(2*i - 1):
+        print('*',end=' ')
+    print("\n")
