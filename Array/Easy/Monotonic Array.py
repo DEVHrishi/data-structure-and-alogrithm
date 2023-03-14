eeda888ffb835f8fdc6dd540f7a30cbdeef950a8
Hrishikesh roy <royhrishikesh408@gmail.com>
2023-03-14 12:11:21 +0530

    commit

--- Array/Easy/Monotonic Array.py
+++ Array/Easy/Monotonic Array.py
@@ -4,10 +4,20 @@ An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An a
 
 Given an integer array nums, return true if the given array is monotonic, or false otherwise.
 
+ 
+
 Example 1:
 
 Input: nums = [1,2,2,3]
-Output: true'''
+Output: true
+Example 2:
+
+Input: nums = [6,5,4,4]
+Output: true
+Example 3:
+
+Input: nums = [1,3,2]
+Output: false'''
 
 def isMonotonic(self, nums ) :
         return nums == sorted(nums) or nums == sorted(nums, reverse=True)
