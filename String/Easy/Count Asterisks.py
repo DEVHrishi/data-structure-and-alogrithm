97496785c60998f45ef7b22bfe4d874d2191bc79
Hrishikesh roy <royhrishikesh408@gmail.com>
2023-03-19 23:40:22 +0530

    commit

--- String/Easy/Count Asterisks.py
+++ String/Easy/Count Asterisks.py
@@ -4,6 +4,8 @@ Return the number of '*' in s, excluding the '*' between each pair of '|'.
 
 Note that each '|' will belong to exactly one pair.
 
+ 
+
 Example 1:
 
 Input: s = "l|*e*et|c**o|*de|"
@@ -11,7 +13,17 @@ Output: 2
 Explanation: The considered characters are underlined: "l|*e*et|c**o|*de|".
 The characters between the first and second '|' are excluded from the answer.
 Also, the characters between the third and fourth '|' are excluded from the answer.
-There are 2 asterisks considered. Therefore, we return 2.'''
+There are 2 asterisks considered. Therefore, we return 2.
+Example 2:
+
+Input: s = "iamprogrammer"
+Output: 0
+Explanation: In this example, there are no asterisks in s. Therefore, we return 0.
+Example 3:
+
+Input: s = "yo|uar|e**|b|e***au|tifu|l"
+Output: 5
+Explanation: The considered characters are underlined: "yo|uar|e**|b|e***au|tifu|l". There are 5 asterisks considered. Therefore, we return 5.'''
 
 
 def countAsterisks(s):
