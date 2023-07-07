f0aa0a1e919afdcfee4c6671960489baa5faf9b4
DEVHrishi <77627038+DEVHrishi@users.noreply.github.com>
2023-07-07 23:53:23 +0530

    commit

---
+++ Greedy_Algorithm/Easy/Maximize Sum Of Array After K Negations.py
@@ -0,0 +1,40 @@
+'''Given an integer array nums and an integer k, modify the array in the following way:
+
+choose an index i and replace nums[i] with -nums[i].
+You should apply this process exactly k times. You may choose the same index i multiple times.
+
+Return the largest possible sum of the array after modifying it in this way.
+
+ 
+
+Example 1:
+
+Input: nums = [4,2,3], k = 1
+Output: 5
+Explanation: Choose index 1 and nums becomes [4,-2,3].
+Example 2:
+
+Input: nums = [3,-1,0,2], k = 3
+Output: 6
+Explanation: Choose indices (1, 2, 2) and nums becomes [3,1,0,2].
+Example 3:
+
+Input: nums = [2,-3,-1,5,-4], k = 2
+Output: 13
+Explanation: Choose indices (1, 4) and nums becomes [2,3,-1,5,4].'''
+
+from ast import List
+
+# tc: O(nlogn) and sc: O(1)
+class Solution:
+    def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
+        nums.sort(key=abs, reverse=True)
+        for i in range(len(nums)):
+            if nums[i] < 0 and k > 0:
+                nums[i] *= -1
+                k -= 1
+            if k == 0:
+                break
+        if k % 2 != 0:
+            nums[-1] *= -1
+        return sum(nums)
