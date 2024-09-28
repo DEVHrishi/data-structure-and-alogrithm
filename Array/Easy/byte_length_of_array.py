6cfb394d0eb14f651ed2aa54b38f1f257c4841d6
Hrishikesh roy <77627038+DEVHrishi@users.noreply.github.com>
2024-09-28 23:23:01 +0530

    removed

--- Array/Easy/byte_length_of_array.py
+++
@@ -1,4 +0,0 @@
-from array import *
-arr_input = array('i', [1, 3, 5, 7, 9])
-print('Original array: '+str(arr_input))
-print("Length in bytes of one array item: "+str(arr_input.itemsize))
--- Array/Easy/search_insert_position.py
+++
@@ -1,8 +0,0 @@
-def search(arr_input, target):
-    for i in range(len(arr_input)):
-        if arr_input[i] == target:
-            return i
-        if arr_input[i] > target:
-            return i
-
-print('search insert position:', search([1,3,5,6], 2))
